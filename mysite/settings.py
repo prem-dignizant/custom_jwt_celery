f9f8313c4343c8548eeeb3710745af02d722bba5
prem-dignizant <prem.rakh@dignizant.com>
2025-01-07 16:26:05 +0530

    allow host to all

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u58bk=bf)n8=neeb($i^f^o@7@g5zk4n17725r1ees01rx7q0v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
